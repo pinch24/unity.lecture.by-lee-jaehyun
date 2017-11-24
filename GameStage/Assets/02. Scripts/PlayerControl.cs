bdc2a287df4956dc68b0bb2d77d66d3a5cf7502c
MK <pinch24@gmail.com>
2017-11-24 14:16:18 +0900

    Character Animation

--- GameStage/Assets/02. Scripts/PlayerControl.cs
+++ GameStage/Assets/02. Scripts/PlayerControl.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class AnimationClipList {
+
+	public AnimationClip idle;
+	public AnimationClip runForward;
+	public AnimationClip runBackward;
+	public AnimationClip runRight;
+	public AnimationClip runLeft;
+}
+
 public class PlayerControl : MonoBehaviour {
 
 	private float h = 0.0f;
 	private float v = 0.0f;
-
-	private Transform tr;
 	
 	public float moveSpeed = 10.0f;
 	public float rotateSpeed = 100.0f;
+
+	public AnimationClipList animationClipList;
+	public Animation animationControl;
 	
+	private Transform tr;
+
 	void Start () {
 		
 		tr = GetComponent<Transform>();
+
+		animationControl = GetComponentInChildren<Animation>();
+
+		animationControl.clip = animationClipList.idle;
+		animationControl.Play();
 	}
 	
 	// Update is called once per frame
@@ -23,11 +41,29 @@ public class PlayerControl : MonoBehaviour {
 		h = Input.GetAxis("Horizontal");
 		v = Input.GetAxis("Vertical");
 
-		Debug.Log("H=" + h.ToString());
-		Debug.Log("V=" + v.ToString());
-
 		Vector3 moveDirection = (Vector3.forward * v) + (Vector3.right * h);
 		tr.Translate(moveDirection.normalized * moveSpeed * Time.deltaTime, Space.Self);
 		tr.Rotate(Vector3.up * rotateSpeed * Time.deltaTime * Input.GetAxis("Mouse X"));
+
+		if (v >= 0.1f) {
+
+			animationControl.CrossFade(animationClipList.runForward.name, 0.3f);
+		}
+		else if (v <= -0.1f) {
+
+			animationControl.CrossFade(animationClipList.runBackward.name, 0.3f);
+		}
+		else if (h >= 0.1f) {
+
+			animationControl.CrossFade(animationClipList.runRight.name, 0.3f);
+		}
+		else if (h <= -0.1f) {
+
+			animationControl.CrossFade(animationClipList.runLeft.name, 0.3f);
+		}
+		else {
+
+			animationControl.CrossFade(animationClipList.idle.name, 0.3f);
+		}
 	}
 }
